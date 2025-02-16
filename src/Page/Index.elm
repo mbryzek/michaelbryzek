de76b698241616184ea0cb4de372cc8d727db9be
Michael Bryzek <mbryzek@alum.mit.edu>
2025-02-16 14:13:02 -0500

    wip

--- src/Page/Index.elm
+++ src/Page/Index.elm
@@ -18,5 +18,9 @@ contents =
     Html.div []
         [ p [] [ text "My name is Michael Bryzek and I'm a developer living in New Jersey. " ]
         , p [class "mt-4"] [ text "I’m a serial entrepreneur, best known for founding Gilt Groupe in 2007 and Flow Commerce in 2015, which now powers international commerce inside Shopify." ]
-        , p [class "mt-4"] [ text "Recently I'm spending most my time building True Acumen, a personal expense and budget management application." ]
+        , p [class "mt-4"] [
+                text "Recently I'm spending most my time building "
+                , externalLinkToAcumen
+                , text " a personal expense and budget management application."
+            ]
         ]
