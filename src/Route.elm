1662317a1f9b05cd3266e9db8c305c6a2efa4f5f
Michael Bryzek <mbryzek@alum.mit.edu>
2025-02-16 12:52:26 -0500

    wip

--- src/Route.elm
+++ src/Route.elm
@@ -2,12 +2,14 @@ module Route exposing (Route(..), fromUrl)
 
 import Url exposing (Url)
 import Url.Parser exposing (..)
+import Urls
 
 
 type Route
     = RouteIndex
     | RouteProjects
     | RouteBlogIndex
+    | RouteBlogPost Urls.BlogPostParams
     | RouteTalks
 
 
@@ -22,5 +24,6 @@ matchRoute =
         [ map RouteIndex top
         , map RouteProjects (s "projects")
         , map RouteBlogIndex (s "blog")
+        , map (\slug -> RouteBlogPost { slug = slug }) (s "blog" </> string)
         , map RouteTalks (s "talks")
         ]
