02c0df1c1bb1d939b1246e2fe84a6eaef793d5fd
Michael Bryzek <mbryzek@alum.mit.edu>
2025-02-17 17:39:52 -0500

    wip

--- src/Posts/StateManagementInElm.elm
+++ src/Posts/StateManagementInElm.elm
@@ -6,5 +6,9 @@ import Posts.Style exposing (blogP)
 
 contents : List (Html msg)
 contents =
-    [ blogP [ text "Managing state in an Elm single-page application (SPA) requires careful structuring. Over time, I’ve refined an approach that balances simplicity and scalability. This post shares my methodology using a minimal example app covering:" ]
+    [ blogP [ text """
+    Managing state in an Elm single-page application (SPA) requires careful structuring.
+    Over time, I’ve refined an approach that balances simplicity and scalability.
+    This post shares my methodology using a minimal example app covering:
+    """]
     ]
