d1bbcafa893f49953cbf354028287fa7699f42ce
Michael Bryzek <mbryzek@alum.mit.edu>
2025-02-17 18:15:18 -0500

    wip

--- src/Posts/StateManagementInElm.elm
+++ src/Posts/StateManagementInElm.elm
@@ -11,6 +11,7 @@ contents =
     List.concat [
         intro
         , authentication
+        , init
     ]
 
 
@@ -55,7 +56,10 @@ authentication : List (Html msg)
 authentication =
     [ blogH2 [ text "Authentication" ]
     , blogP [ text """
-    I've found that it has been helpful to handle most of Authentication in Main.elm. My approach has been to define a type "GlobalState" which tracks authentication status:
+    I've found that it has been helpful to handle most of Authentication in Main.elm.
+    My approach has been to define a type "GlobalState" which tracks authentication status.
+    In all cases, we keep track of the Nav Key and the current URL. If a user is
+    successfully authenticated, we also keep track of the Session.
     """ ]
     , blogCode """
 type GlobalState
@@ -75,4 +79,67 @@ type alias GlobalStateAuthenticatedData =
     , session : Session
     }
     """
+    , blogP [ text """
+    Within Main, we need to first initialize the state and then route to the appropriate page.
+    This also means that we can centralize the logic on when to ask the user to login or register directly
+    in Main.
+    """ ]
+    , blogP [ text """
+    To handle initialization, we declare the Model in Main as either being in the init state (InitModel) or Ready (ReadyModel).
+    Note here you will also see our Shell.Model introduced which will store all of the state needed for the Template.
+    """ ]
+    , blogCode """
+type Model
+    = Init InitModel
+    | Ready ReadyModel
+
+
+type alias InitModel =
+    { sessionId : String
+    , url : Url.Url
+    , key : Nav.Key
+    }
+
+
+type alias ReadyModel =
+    { global : GlobalState
+    , shellModel : Shell.Model
+    , page : Page
+    }
+"""
+    , blogP [ text """
+    In Main, if we are provided a Session ID, we will then load the session and upon completion continue with a ready model.
+
+    """]
     ]
+
+
+
+init : List (Html msg)
+init =
+    [ blogH2 [text "Page Initialization"]
+    , blogP [ text """
+    In page initialization, we keep track of whether or not a particular page requires the user to have
+    already authenticated. If so, we check authentication and either redirect to the login page or
+    provide the authenticated data to the page.
+    """ ]
+    , blogCode """
+pageAuthenticatedData : GlobalState -> (GlobalStateAuthenticatedData -> ( Page, Cmd PageMsg )) -> ( Page, Cmd PageMsg )
+pageAuthenticatedData global f =
+    case global of
+        GlobalStateAuthenticated data ->
+            f data
+
+	_ ->
+            ( PageNotAuthorized, Nav.pushUrl (Global.getNavKey global) "/login" )
+    """
+    , blogCode """
+getPageFromRoute : GlobalState -> Maybe Route -> ( Page, Cmd PageMsg )
+getPageFromRoute global maybeRoute =
+    case maybeRoute of
+    	Just Route.RouteRestricted ->
+            pageAuthenticatedData global (\\g ->
+                ( PageRestricted.init g |> PageRestricted, Cmd.none)
+"""
+    ]
+
