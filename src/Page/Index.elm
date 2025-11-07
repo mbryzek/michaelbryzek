4f6472548fa20bfc85ae743d329472ab4264fdff
Michael Bryzek <mbryzek@alum.mit.edu>
2025-11-07 17:44:12 -0500

    Fix grammar

--- src/Page/Index.elm
+++ src/Page/Index.elm
@@ -19,7 +19,7 @@ contents =
         [ p [] [ text "My name is Michael Bryzek and I'm a developer and serial entrepreneur living in New Jersey. " ]
         , p [class "mt-4"] [ text "I’m best known for founding Gilt Groupe in 2007 and Flow Commerce in 2015, which now powers international commerce inside Shopify." ]
         , p [class "mt-4"] [
-                text "Recently, I spend most my time building "
+                text "Recently, I spend most of my time building "
                 , externalLink [] "https://privatedinkers.com" [ text "Private Dinkers" ]
                 , text ", an AI-powered pickleball game scheduling application."
             ]
